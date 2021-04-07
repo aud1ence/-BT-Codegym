f85fdebb940b38b08aef05b176384628c03b0181
aud1ence <81702063+aud1ence@users.noreply.github.com>
2021-04-07 10:18:44 +0700

    done

---
+++ BP-p7/BT-chuyen-doi-tien-te/chuye-tien.js
@@ -0,0 +1,7 @@
+function convert() {
+    let input = document.getElementById("dauVao").value;
+    let to = document.getElementById("to").value;
+    let from = document.getElementById("from").value;
+    result = input * to / from;
+    document.getElementById('ketqua').innerHTML=result;
+}
